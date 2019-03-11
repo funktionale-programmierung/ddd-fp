2d34afe228073ed70b8aac63d3ecb2c260140e0a
Mike Sperber <sperber@deinprogramm.de>
2019-03-11 11:00:22 +0100

    Nix setup.

---
+++ code/default.nix
@@ -0,0 +1,12 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+let
+  inherit (nixpkgs) pkgs;
+  ghc = nixpkgs.haskellPackages.ghcWithPackages (ps: with ps; [
+          groups
+        ]);
+in
+pkgs.stdenv.mkDerivation {
+  name = "dddfp";
+  buildInputs = [ ghc ];
+  shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
+}
