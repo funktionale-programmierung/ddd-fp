c8c84fd7fcd90838d45fc3889d73820ffb64093a
Mike Sperber <sperber@deinprogramm.de>
2019-03-12 16:35:44 +0100

    Application layer

--- code/ShowerProductNeu.hs
+++ code/ShowerProductNeu.hs
@@ -1,7 +1,7 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 import Data.Map as Map
 import qualified Data.Map.Strict (Map)
@@ -78,8 +78,8 @@ istVorratKorrekt :: Vorrat -> Bool
 istVorratKorrekt (Vorrat vorrat) =
   Map.foldr (\ (Menge menge) istKorrekt -> istKorrekt && (menge >= 0)) True vorrat
 
-newtype Farbe = Farbe String deriving (Eq, Show, Ord)
-newtype PH = PH Double deriving (Eq, Show, Ord)
+data Farbe = Farbe String deriving (Eq, Show, Ord)
+data PH = PH Double deriving (Eq, Show, Ord)
 
 data Haartyp = Fettig | Trocken | Normal | Schuppen
   deriving (Eq, Show, Ord)
@@ -94,7 +94,7 @@ data WaschProdukt =
   | Gemisch Double WaschProdukt Double WaschProdukt
   deriving (Eq, Show, Ord)
 
-newtype Id = Id Int deriving (Eq, Show)
+data Id = Id Int deriving (Eq, Show)
 
 data Entitaet daten = Entitaet Id daten
   deriving (Show)
@@ -102,11 +102,11 @@ data Entitaet daten = Entitaet Id daten
 instance Eq (Entitaet a) where
   (Entitaet id1 _) == (Entitaet id2 _) = id1 == id2
 
-newtype Menge = Menge Double deriving (Eq, Show, Ord)
+data Menge = Menge Double deriving (Eq, Show, Ord)
 
 leereMenge = Menge 0
 
-newtype ProduktName = ProduktName String
+data ProduktName = ProduktName String
   deriving (Eq, Show, Ord)
 
 data BestellDaten = BestellDaten ProduktName Menge
@@ -116,11 +116,21 @@ type Bestellung = Entitaet BestellDaten
 
 type Katalog = Map ProduktName WaschProdukt
 
-newtype Vorrat = Vorrat (Map Grundbestandteil Menge)
-  deriving (Eq, Show, Monoid, Semigroup)
+data Vorrat = Vorrat (Map Grundbestandteil Menge)
+  deriving (Eq, Show)
+
+instance Semigroup Vorrat where
+  (Vorrat map1) <> (Vorrat map2) =
+    Vorrat (unionWith (<>) map1 map2)
+
+instance Monoid Vorrat where
+  mempty = leererVorrat
 
 leererVorrat = Vorrat Map.empty
 
+vorratAus :: Grundbestandteil -> Menge -> Vorrat
+vorratAus grundbestandteil menge =
+  Vorrat (Map.fromList [(grundbestandteil, menge)])
 
 data Event =
     BestellungAkzeptiert Bestellung
@@ -152,4 +162,69 @@ bestellungShampoo = Entitaet (Id 2) (BestellDaten (ProduktName "Schuppenshampoo"
 bestellungShampooZuViel = Entitaet (Id 3) (BestellDaten (ProduktName "Schuppenshampoo") (Menge 20))
 bestellungUnbekannt = Entitaet (Id 4) (BestellDaten (ProduktName "Erdbeermilch") (Menge 1))
 
+bestelldatenUnbekannt = BestellDaten (ProduktName "Erdbeermilch") (Menge 1)
+bestelldatenShampoo = BestellDaten (ProduktName "Schuppenshampoo") (Menge 1)
+
+eventEffektAufVorrat :: Vorrat -> Event -> Vorrat
+eventEffektAufVorrat vorrat (GrundbestandteilEntnommen bestellung grundbestandteil menge) =
+  vorrat <> (invert (vorratAus grundbestandteil menge))
+eventEffektAufVorrat vorrat _ = vorrat
+
+eventsEffektAufVorrat :: Vorrat -> [Event] -> Vorrat
+eventsEffektAufVorrat vorrat events =
+  Prelude.foldr (\ event vorrat ->
+           eventEffektAufVorrat vorrat event)
+    vorrat events
+
+-- Version mit Repository / Monade
+
+verarbeiteBestellung' :: InterfaceIdGenerator m => BestellDaten -> Vorrat -> Katalog -> m [Event]
+verarbeiteBestellung' bestelldaten aktuellerVorrat katalog =
+  do bestellung <- baueEntitaet bestelldaten
+     return ([BestellungAkzeptiert bestellung] ++ (findeProduktMischeUndVersendeBestellung bestellung aktuellerVorrat katalog))
+
+class Monad m => InterfaceIdGenerator m where
+  newId :: m Id
+
+------ Application Layer
+
+baueEntitaet :: InterfaceIdGenerator m => d -> m (Entitaet d)
+baueEntitaet daten =
+  do id <- newId
+     return (Entitaet id daten)
+
+data IdGenerator a = IdGenerator (Int -> (Int, a))
+
+runIdGenerator (IdGenerator idgf) n = idgf n
+
+instance InterfaceIdGenerator IdGenerator where
+  newId :: IdGenerator Id
+  newId = IdGenerator (\ n -> (n+1, Id n))
+
+instance Monad IdGenerator where
+  (>>=)  :: IdGenerator a -> (  a -> IdGenerator b) -> IdGenerator b
+  (IdGenerator idgf) >>= f =
+    IdGenerator (\ n ->
+                   let (n', aelem) = idgf n
+                       (IdGenerator idgf') = f aelem
+                   in idgf' n')
+  return ::  a -> IdGenerator a
+  return x = IdGenerator (\ n -> (n, x))
+
+instance Applicative IdGenerator where
+  pure :: a -> IdGenerator a
+  pure x = IdGenerator (\ n -> (n, x))
+  (<*>) :: IdGenerator (a -> b) -> IdGenerator a -> IdGenerator b
+  (IdGenerator f) <*> (IdGenerator idgf) =
+    IdGenerator (\ n ->
+                   let (n', f') = f n
+                       (n'', aelem) = idgf n'
+                   in (n'', f' aelem))
+
+instance Functor IdGenerator where
+  fmap :: (a -> b) -> IdGenerator a -> IdGenerator b
+  fmap f (IdGenerator idgf) =
+    IdGenerator (\ n ->
+                   let (n', aelem) = idgf n
+                   in (n', f aelem))
 
